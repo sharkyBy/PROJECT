93d3e452b86abd2d074b71faacedc99dc7547fd0
sharkyBy <valik_1973@mail.ru>
2019-12-27 22:32:29 +0300

    add file's

--- js/main.js
+++ js/main.js
@@ -2,14 +2,18 @@ let btn = document.getElementsByClassName('header__nav-btn')[0];
 console.log(btn);
 btn.addEventListener('click',clickBtn);
 let menu = document.getElementsByClassName('header__nav-menu')[0];
-console.log(menu)
+console.log(menu);
+let span = btn.firstElementChild;
 
 function clickBtn(event){    
-    this.classList.toggle('header__nav-btn_active');    
-    this.firstElementChild.classList.toggle('header__nav-btn_active')
+    this.classList.toggle('header__nav-btn-active');    
+    this.firstElementChild.classList.toggle('header__nav-btn-active');
+    span.style.background != "transparent" ?  span.style.background = "transparent":  span.style.background = "white";
     
-    for(let i = 0; i < menu.children.length; i++){        
-    menu.children[i].classList.toggle('header__nav-menu-active');    
+    for(let i = 0; i < menu.children.length; i++){
+             
+    menu.children[i].classList.toggle('header__nav-menu-active');
+        
     }
     
 }
