1907612617d21f0ac7afd3088a1381ba2ee426bf
sharkyBy <valik_1973@mail.ru>
2019-12-30 17:08:50 +0300

    odificy

--- js/main.js
+++ js/main.js
@@ -7,9 +7,8 @@ let span = btn.firstElementChild;
 
 function clickBtn(event){    
     this.classList.toggle('header__nav-btn-active');    
-    this.firstElementChild.classList.toggle('header__nav-btn-active');
-    span.style.background != "transparent" ?  span.style.background = "transparent":  span.style.background = "white";
-    
+    this.firstElementChild.classList.toggle('header__nav-btn-active');    
+    // span.classList.contains('header__nav-btn-active') ? span.style.background = "transparent": span.style.background = "white";
     for(let i = 0; i < menu.children.length; i++){
              
     menu.children[i].classList.toggle('header__nav-menu-active');
