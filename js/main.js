b6bb86682e0685e91179deac05556ba0e98d28b9
sharkyBy <valik_1973@mail.ru>
2020-01-06 18:55:31 +0300

    modified any files

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-let btn = document.getElementsByClassName('header__nav-btn')[0];
+let btn = document.getElementsByClassName('header__nav_wrap')[0];
 console.log(btn);
 btn.addEventListener('click',clickBtn);
-let menu = document.getElementsByClassName('header__nav-menu')[0];
+let menu = document.getElementsByClassName('header__nav_menu')[0];
 console.log(menu);
-let span = btn.firstElementChild;
+// let span = btn.firstElementChild;
 
 function clickBtn(event){    
-    this.classList.toggle('header__nav-btn-active');    
-    this.firstElementChild.classList.toggle('header__nav-btn-active');    
+    this.classList.toggle('header__nav_btn-active');    
+    // this.firstElementChild.classList.toggle('header__nav_btn-active');    
     // span.classList.contains('header__nav-btn-active') ? span.style.background = "transparent": span.style.background = "white";
     for(let i = 0; i < menu.children.length; i++){
              
-    menu.children[i].classList.toggle('header__nav-menu-active');
+    menu.children[i].classList.toggle('header__nav_menu-active');
         
     }
     
