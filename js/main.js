e3dd146efa90567ef623a6947503ad17fc71bd3f
sharkyBy <valik_1973@mail.ru>
2020-01-08 23:04:59 +0300

    added my personal  detals

--- js/main.js
+++ js/main.js
@@ -1,39 +1,58 @@
+// nav button
+
 let btn = document.getElementsByClassName('header__nav_wrap')[0];
 console.log(btn);
 btn.addEventListener('click',clickBtn);
 let menu = document.getElementsByClassName('header__nav_menu')[0];
 console.log(menu);
-// let span = btn.firstElementChild;
+
 
 function clickBtn(event){    
     this.classList.toggle('header__nav_btn-active');    
     // this.firstElementChild.classList.toggle('header__nav_btn-active');    
     // span.classList.contains('header__nav-btn-active') ? span.style.background = "transparent": span.style.background = "white";
-    for(let i = 0; i < menu.children.length; i++){
-             
+    for(let i = 0; i < menu.children.length; i++){             
     menu.children[i].classList.toggle('header__nav_menu-active');
         
     }
     
 }
 
+
+
 //The input name animation
 
 let form = document.getElementsByTagName('form')[0];
-let input = form.querySelectorAll('input');
 
-for( let i =0; i < input.length;i++){
+let input = form.querySelectorAll('input');
+let textArea = form.querySelector('textarea');
+
+textArea.onfocus = function(){
+    form.children[2].classList.add('clickInput');
+};
+
+for( let i =0; i < input.length - 1;i++){
     input[i].onfocus = function(e){
-        form.children[i].classList.add('clickInput')
+        form.children[i].classList.add('clickInput');
         console.log(e.target);
-    }
+        
+    }    
 }
 
 
 
+/* for(let i = 0; i < icon.length; i++) {
+    icon[i].onfocus = function (e) {
+        console.log(icon[i]);
+      icon[i].children[0].children[1].toogle('briefly'); 
+    console.log (e.target);  
+    }
+}   */ 
 
 
-// для вращения 3д карточек 
+
+
+// for 3d cards rotate 
 /* 
 let next = document.getElementById('nextBtn');
 next.addEventListener('click',clickNext);
